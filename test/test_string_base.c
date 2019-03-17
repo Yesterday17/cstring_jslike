75310ffbe21fd55e8acb99fdb5a00c305591baf8
KAAAsS <admin@kaaass.net>
2019-03-17 22:09:11 +0800

    Complete unit test for base function

--- test/test_string_base.c
+++ test/test_string_base.c
@@ -10,6 +10,10 @@
 void test_create_string() {
   string test = LITERAL("Test");
   TEST_ASSERT_MESSAGE(test != NULL, "String is not created.");
+
+  stringbuf testBuf = STR_BUF("TestBuf");
+  TEST_ASSERT_MESSAGE(testBuf != NULL, "String is not created.");
+  $STR_BUF(testBuf);
 }
 
 void test_compareString() {
@@ -31,11 +35,27 @@ void test_compareString() {
   TEST_ASSERT_EQUAL_INT(STRING_LARGER, compareString(inte, u8B));
 }
 
+void test_reverseString() {
+  TEST_ASSERT(EQ(LITERAL("abcde"), LITERAL("edcba")));
+  TEST_ASSERT(EQ(LITERAL("1919"), LITERAL("9191")));
+  // TODO: Add test case for UTF-8 string
+}
+
+void test_cloneString() {
+  string orgStr = LITERAL("Origin");
+  stringbuf newStr = cloneString(orgStr);
+  TEST_ASSERT(orgStr != newStr);
+  TEST_ASSERT(EQ(orgStr, newStr));
+  $STR_BUF(newStr);
+}
+
 int main() {
   UNITY_BEGIN();
 
   RUN_TEST(test_create_string);
   RUN_TEST(test_compareString);
+  RUN_TEST(test_reverseString);
+  RUN_TEST(test_cloneString);
 
   return UNITY_END();
 }
